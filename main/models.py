7c7a37a7fe7dfc2d4d50646250e519f5b091f1b6
Ali <Alihosseinzadeh1379ahz@gmail.com>
2022-06-30 11:35:37 +0430

    models created for 'RSS' and 'News'

--- main/models.py
+++ main/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Rss(models.Model):
+    press_name = models.CharField(max_length=200)
+    link = models.URLField(max_length=200)
+
+    class Meta:
+        verbose_name = 'RSS'
+        verbose_name_plural = 'RSSs'
+
+    def __str__(self):
+        return self.press_name
+
+
+
+class News(models.Model):
+    title = models.CharField(max_length=300)
+    link = models.URLField(max_length=200)
+    description = models.TextField()
+    author = models.CharField(max_length=200)
+    publish_date = models.DateTimeField()
+
+    class Meta:
+        ordering = ['-publish_date']
+        verbose_name_plural = 'News'
+
+    def __str__(self):
+        return self.title
