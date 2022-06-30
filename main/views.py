3c0601a467fd5085fc2ad5b8fb6e5afef039e4e1
Ali <Alihosseinzadeh1379ahz@gmail.com>
2022-06-30 21:13:19 +0430

    pagination added

--- main/views.py
+++ main/views.py
@@ -2,12 +2,20 @@ from rest_framework import mixins
 from rest_framework import generics
 from main.models import News
 from main.serializers import NewsListSerializer
+from rest_framework.pagination import PageNumberPagination
+
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 15
+    page_size_query_param = 'page_size'
+    max_page_size = 500
 
 
 class NewsListView(mixins.ListModelMixin, generics.GenericAPIView):
 
     queryset = News.objects.all()
     serializer_class = NewsListSerializer
+    pagination_class = StandardResultsSetPagination
 
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
